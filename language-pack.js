995545006c9d5275defa944eb16b601b5ce7e956
gdev219 <gdev219@gmail.com>
2018-10-18 20:50:27 +0900

    Add files via upload

---
+++ language-pack.js
@@ -0,0 +1,24 @@
+export default {
+  messages: {
+    en: {
+      menu: {
+        hello: 'hello world!@#'
+      }
+    },
+    ja: {
+      menu: {
+        hello: 'こんにちは、世界'
+      }
+    },
+    ko: {
+      menu: {
+        notice: '공지2',
+        board: '게시판',
+        hello: '안녕하세요'
+      },
+      test: {
+        notice: '공지1'
+      }
+    }
+  }
+};
